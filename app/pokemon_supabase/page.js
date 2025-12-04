3f3a0f1ff7c593816245bcc09ed0407403cf5f59
Massil <160241880+Massil-br@users.noreply.github.com>
2025-12-04 16:40:22 +0100

    corrected errors

--- app/pokemon_supabase/page.js
+++ app/pokemon_supabase/page.js
@@ -2,13 +2,14 @@ import { GetAllPokemons } from "@/services/pokemonService";
 
 export default async function PokemmonSupabasePage(){
     const pokemons = await GetAllPokemons();
+    console.log(pokemons);
     return (
         <div>
             <h1>Pokemon Supabase</h1>
             <ul>
-                {pokemons.map((pokemon) =>{
-                    <li key={pokemon.id_pokedex}>{pokemon.name}</li>
-                })}
+                {pokemons.map((pokemon) =>(
+                    <li key={pokemon.pokedex_id}>{pokemon.name}</li>
+                ))}
             </ul>
         </div>
     )
